aced0c87385d7891442c9561042b3ad2da773dca
Юрий <fedejkin@gmail.com>
2024-08-20 20:01:54 +0300

    Inicial Commit

---
+++ src/main/java/pro/sky/algorithmsLesson/AlgorithmsLessonApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.algorithmsLesson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmsLessonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgorithmsLessonApplication.class, args);
+	}
+
+}
